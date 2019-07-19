fc9b44b567fc11dd0b7bf0abc1407e4fc0f312f8
Michael J Maggs <michaelmaggs@gmail.com>
2019-07-18 17:51:46 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+function getTask() {
+    let form = document.querySelector("#create-task-form")
+    form.addEventListener("submit", function(e) {
+        e.preventDefault()
+        task = document.getElementById('new-task-description').value
+        addTask(task)
+        task = ""
+    })
+}
+
+ function addTask(arg) {
+     let ul = document.getElementById('tasks')
+     let li = document.createElement('li')
+     li.innerText = arg
+     li.style.fontSize = "x-small"
+     let button = document.createElement('button')
+     button.innerText = "x"
+     ul.appendChild(li)
+     li.appendChild(button)
+     deleteTask()
+ }
+
+ function deleteTask() {
+    allLis = document.querySelectorAll('li')
+    console.log("hello")
+    allLis.forEach (li => {
+       let button = li.querySelector('button')
+       button.addEventListener("click", function(e) {
+         li.remove()
+       }) 
+    })
+ }
+
+ getTask()
